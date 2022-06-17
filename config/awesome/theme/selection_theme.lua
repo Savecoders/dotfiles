0aa47fc821bc60e7841750e158d4d50f4de13042
Savecoders <pablopincay2003@gmail.com>
2022-06-16 20:48:50 -0500

    change selection theme

--- config/awesome/theme/selection_theme.lua
+++ config/awesome/theme/selection_theme.lua
@@ -8,18 +8,8 @@ local rubato = require("modules.rubato")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 local helpers = require("helpers")
+local gfs = require('gears.filesystem')
 
--- icons
-local icons = {
-    dark = {
-        outline = "",
-        glyph = ""
-    },
-    light = {
-        outline = "",
-        glyph = "盛"
-    }
-}
 
 local change_dark_theme = function()
     awful.spawn.with_shell(gears.filesystem.get_configuration_dir() .. "scripts/themes/dark")
@@ -31,20 +21,41 @@ local change_light_theme = function()
     awesome.restart()
 end
 
-local cr_btn = require("widgets.button").create
-local dark = cr_btn(icons.dark.outline, dpi(100), dpi(0), beautiful.icon_var .. "22", beautiful.bg, beautiful.icon_normal, beautiful.red,
-    change_dark_theme)
-local light = cr_btn(icons.light.outline, dpi(100), dpi(0), beautiful.icon_var .. "22", beautiful.bg, beautiful.icon_normal, beautiful.red,
-    change_light_theme)
+
+
+-- search image for rice
+
+local dark_img = gfs.get_configuration_dir() .. "wallpapers/dark/darkmode.jpg"
+local light_img = gfs.get_configuration_dir() .. "wallpapers/light/lightmode.jpg"
+
+
+-- create img container
+
+local create_img = require("widgets.button").create_image_onclick
+
+-- containers
+
+local dark = create_img(dark_img, dark_img, change_dark_theme)
+local light = create_img(light_img, light_img, change_light_theme)
+
+-- size
+
+dark.forced_height = dpi(180)
+light.forced_height = dpi(180)
+
+dark.forced_width = dpi(360)
+light.forced_width = dpi(360)
+
 
 local theme_screen = wibox({
-    type = "popup_menu",
-    height = dpi(120),
-    width = dpi(280),
-    shape = helpers.rrect(beautiful.border_radius),
-    bg = "#00000000", -- active
-	ontop   = true,
+    height  = dpi(240),
+    width   = dpi(770),
+	shape   = helpers.rrect(beautiful.border_radius),
+	bg      = beautiful.bg,
+	halign  = "center",
+	valign  = "center",
 	visible = false,
+	ontop 	= true,
 })
 
 awful.placement.centered(theme_screen, {
@@ -54,42 +65,29 @@ awful.placement.centered(theme_screen, {
 })
 
 theme_screen:setup {
-	nil,
 	{
-		nil,
+
 		{
-			{
-				{
-					
-					{
-						dark,
-						light,
-						spacing = dpi(20),
-						layout = wibox.layout.fixed.horizontal
-					},
-					layout = wibox.layout.fixed.horizontal,
-					spacing = dpi(20)
-				},
-				widget = wibox.container.margin,
-				margins = dpi(15)
-			},
-			widget = wibox.container.background,
-			bg = beautiful.bg,
-			shape = helpers.rrect(beautiful.bar_radius)
+			dark,
+			spacing = dpi(20),
+			layout = wibox.layout.fixed.horizontal
 		},
-		layout = wibox.layout.align.vertical,
-		expand = "none"
+		{
+			light,
+			spacing = dpi(20),
+			layout = wibox.layout.fixed.horizontal
+		},
+		layout = wibox.layout.fixed.horizontal,
+		spacing = dpi(20)
 	},
-	expand = "none",
-	layout = wibox.layout.align.horizontal
+	widget = wibox.container.margin,
+	margins = dpi(20)
 }
 
 theme_toogle  = function()
     if theme_screen.visible == false then
         theme_screen.visible = not theme_screen.visible
-        theme_screen.target = dpi(130)
     else
-        theme_screen.target = -dpi(140)
         theme_screen.visible = false
     end
 end
