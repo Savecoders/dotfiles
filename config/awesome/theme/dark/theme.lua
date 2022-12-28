939767b361925465142c21033d42b7ca8202b23a
Savecoders <pablopincay2003@gmail.com>
2022-12-27 23:57:59 -0500

    add new wallpapers

--- config/awesome/theme/dark/theme.lua
+++ config/awesome/theme/dark/theme.lua
@@ -22,7 +22,7 @@ theme.font_screen = "SF Pro Display Medium  "
 
 theme.dir = string.format('%s/.config/awesome/theme', os.getenv('HOME'))
 
-theme.wallpaper = gfs.get_configuration_dir() .. "wallpapers/dark/awesome.jpg"
+theme.wallpaper = gfs.get_configuration_dir() .. "wallpapers/dark/mountains_sky_clouds.jpg"
 
 theme.useless_gap = 10
 theme.useless_less = 8
