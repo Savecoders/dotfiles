78e318426c0e196a7aa35643367026b68274d4b3
Savecoders <pablopincay2003@gmail.com>
2024-08-20 20:08:45 -0500

    Update wallpapers for dark theme

--- config/awesome/theme/dark/theme.lua
+++ config/awesome/theme/dark/theme.lua
@@ -22,7 +22,7 @@ theme.font_screen        = "SF Pro Display Medium  "
 
 theme.dir                = string.format('%s/.config/awesome/theme', os.getenv('HOME'))
 
-theme.wallpaper          = "~/Pictures/Wallpapers/dark/programming.png"
+theme.wallpaper          = "~/Pictures/Wallpapers/dark/house_in_ocean.png"
 
 theme.useless_gap        = 6
 theme.useless_less       = 8
