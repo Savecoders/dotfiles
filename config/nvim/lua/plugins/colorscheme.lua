df4e567edd11df2e5cd012a4dcbfc73f92cd8f92
Savecoders <pablopincay2003@gmail.com>
2024-09-24 22:52:28 -0500

    chore: add other themes in template

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,102 @@
 return {
-	{
-		"sainnhe/sonokai",
-		priority = 1000,
-		config = function()
-			vim.g.sonokai_transparent_background = "1"
-			vim.g.sonokai_enable_italic = "1"
-			vim.g.sonokai_style = "andromeda"
-			vim.cmd.colorscheme("sonokai")
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+      flavour = "mocha",
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+    },
+  },
+  {
+    "sainnhe/sonokai",
+    name = "sonokai",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "andromeda"
+    end,
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+    name = "modus",
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+    opts = {
+      transparent = true,
+      theme = "dragon",
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+        }
+      end,
+    },
+  },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
+  { "rktjmp/lush.nvim", dependencies = { "mcchrish/zenbones.nvim" } },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  { "NTBBloodbath/doom-one.nvim", name = "doom-one" },
+  { "AhmedAbdulrahman/aylin.vim", name = "aylin" },
+  {
+    "xiyaowong/transparent.nvim",
+  },
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+  },
+  {
+    "dgox16/oldworld.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- change this line to change the color scheme
+      colorscheme = "sonokai",
+    },
+  },
 }
