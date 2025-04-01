814e49ec53ea1d0ad0bff578c22df718fcb416b9
Savecoders <pablopincay2003@gmail.com>
2025-04-01 11:58:56 -0500

    feat: update wallpapers for dark, gruvbox, and light themes

--- config/awesome/theme/dark/theme.lua
+++ config/awesome/theme/dark/theme.lua
@@ -22,7 +22,7 @@ theme.font_screen        = "SF Pro Display Medium  "
 
 theme.dir                = string.format('%s/.config/awesome/theme', os.getenv('HOME'))
 
-theme.wallpaper          = "~/Pictures/Wallpapers/dark/house_in_ocean.png"
+theme.wallpaper          = "~/Pictures/Wallpapers/dark/see_market.png"
 
 theme.useless_gap        = 6
 theme.useless_less       = 8
--- config/awesome/theme/gruvbox/theme.lua
+++ config/awesome/theme/gruvbox/theme.lua
@@ -22,7 +22,7 @@ theme.font_screen        = "SF Pro Display Medium  "
 
 theme.dir                = string.format('%s/.config/awesome/theme', os.getenv('HOME'))
 
-theme.wallpaper          = "~/Pictures/Wallpapers/gruvbox/pc_boy.png"
+theme.wallpaper          = "~/Pictures/Wallpapers/gruvbox/japan.png"
 
 theme.useless_gap        = 6
 theme.useless_less       = 8
