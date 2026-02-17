a16967d3842aede82b5a5182107a77935bdcf027
Savecoders <pablopincay2003@gmail.com>
2026-02-16 23:38:54 -0500

    feat(lazygit): add initial configuration for LazyGit plugin

---
+++ config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+  },
+}
