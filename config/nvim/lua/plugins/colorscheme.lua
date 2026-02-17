ca6b433c1b8844dc3e4240ab33a486a46756a924
Savecoders <pablopincay2003@gmail.com>
2026-02-16 23:34:21 -0500

    feat(colorscheme): add support for transparent themes and blur thx gentlemanprogramming

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,82 @@
 return {
-  -- add solarized transparent theme
   {
-    "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd([[ colorscheme NeoSolarized ]])
-    end,
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000,
+      config = function()
+        local transparent = true
+        require("tokyonight").setup({
+          style = "storm",
+          transparent = transparent,
+          term_colors = true,
+          styles = {
+            sidebars = transparent and "transparent" or "dark",
+            floats = transparent and "transparent" or "dark",
+          },
+      })
+        vim.cmd("colorscheme tokyonight")
+        end,
+    },
+    {
+      "Gentleman-Programming/gentleman-kanagawa-blur",
+      name = "gentleman-kanagawa-blur",
+      priority = 1000,
+    },
+    {
+      "rebelot/kanagawa.nvim",
+      priority = 1000,
+      lazy = true,
+      config = function()
+        require("kanagawa").setup({
+          compile = false, -- enable compiling the colorscheme
+          undercurl = true, -- enable undercurls
+          commentStyle = { italic = true },
+          functionStyle = {},
+          keywordStyle = { italic = true },
+          statementStyle = { bold = true },
+          typeStyle = {},
+          transparent = true, -- do not set background color
+          dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+          terminalColors = true, -- define vim.g.terminal_color_{0,17}
+          colors = { -- add/modify theme and palette colors
+            palette = {},
+            theme = {
+              wave = {},
+              lotus = {},
+              dragon = {},
+              all = {
+                ui = {
+                  bg_gutter = "none", -- set bg color for normal background
+                  bg_sidebar = "none", -- set bg color for sidebar like nvim-tree
+                  bg_float = "none", -- set bg color for floating windows
+                },
+              },
+            },
+          },
+          overrides = function(colors) -- add/modify highlights
+            return {
+              LineNr = { bg = "none" },
+              NormalFloat = { bg = "none" },
+              FloatBorder = { bg = "none" },
+              FloatTitle = { bg = "none" },
+              TelescopeNormal = { bg = "none" },
+              TelescopeBorder = { bg = "none" },
+              LspInfoBorder = { bg = "none" },
+            }
+          end,
+          theme = "wave", -- Load "wave" theme
+          background = { -- map the value of 'background' option to a theme
+            dark = "wave", -- try "dragon" !
+            light = "lotus",
+          },
+        })
+      end,
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "gentleman-kanagawa-blur",
+      },
+    },
   },
 }
