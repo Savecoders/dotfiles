64361e5b2f4c8173722702fdf39fba885c559835
Savecoders <pablopincay2003@gmail.com>
2026-07-11 10:35:20 -0500

    feat(hypr): zen browser picture in picture

--- config/hypr/config/rules.lua
+++ config/hypr/config/rules.lua
@@ -9,15 +9,37 @@
 -- Vicinae rules
 -- extract to vicinae hyprland quickstart conf https://docs.vicinae.com/quickstart/hyprland
 hl.layer_rule({
-  name = "vicinae-blur",
-  match = { namespace = "vicinae" },
-  blur = true,
-  ignore_alpha = 0
+	name = "vicinae-blur",
+	match = { namespace = "vicinae" },
+	blur = true,
+	ignore_alpha = 0,
 })
 
 -- disable animation for vicinae only
 hl.layer_rule({
-  name = "vicinae-no-animation",
-  match = { namespace = "vicinae" },
-  no_anim = true
+	name = "vicinae-no-animation",
+	match = { namespace = "vicinae" },
+	no_anim = true,
 })
+
+-- file manager
+hl.window_rule({
+	match = {
+		class = "^(thunar)$",
+	},
+	float = true,
+})
+
+-- Zen browser picture in picture float
+hl.window_rule({
+	match = {
+		class = "^(zen-browser)$",
+		title = "^(Picture-In-Picture)$",
+	},
+	float = true,
+})
+--hl.window_rule({
+--	match = { class = "^(steam)$", title = "^(notificationtoasts)" },
+--	no_initial_focus = true,
+--	pin = true,
+--})
