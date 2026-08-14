017b1833cfe0a308f10c21fbb2e2ab8e15494290
Savecoders <pablopincay2003@gmail.com>
2026-08-13 23:41:56 -0500

    refactor(qtct): replace Fusion to Breeze deault theme and load new color scheme

--- config/hypr/config/environment.lua
+++ config/hypr/config/environment.lua
@@ -20,6 +20,7 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- Themes
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_QPA_PLATFORMTHEME_QT6", "qt6ct")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
